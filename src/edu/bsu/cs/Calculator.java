86184ad0a2f12c0203a397e4bcbdbd1886228423
Noah Connolly <ndconnolly@bsu.edu>
2020-09-09 19:58:22 -0400

    Added Functions binarySqRoot & binarySqrd ToDo: Add Unit Tests and confirm functionality.

--- src/edu/bsu/cs/Calculator.java
+++ src/edu/bsu/cs/Calculator.java
@@ -4,4 +4,16 @@ public class Calculator {
 
     //TODO: Write application logic
 
+    public String binarySqRoot(String binaryInput){
+        int binaryToDecimal = Integer.parseInt(binaryInput, 2);
+        String binarySqRoot = Integer.toBinaryString((int)Math.sqrt(binaryToDecimal));
+        return binarySqRoot;
+    }
+
+    public String binarySqrd( String binaryInput){
+        int binaryToDecimal = Integer.parseInt(binaryInput, 2);
+        String binarySqrd = Integer.toBinaryString(binaryToDecimal * binaryToDecimal);
+        return binarySqrd;
+    }
+
 }
