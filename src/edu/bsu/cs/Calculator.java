2e5ff88522c4578d3b1390ec2a9b589e3b34bc1f
Nathaniel Barber <ncbarber@bsu.edu>
2020-09-10 17:19:20 -0400

    Added Functions binarySubtract and binaryAddition

--- src/edu/bsu/cs/Calculator.java
+++ src/edu/bsu/cs/Calculator.java
@@ -16,4 +16,18 @@ public class Calculator {
         return binarySqrd;
     }
 
+    public String binarySubtract(String firstBinary, String secondBinary){
+        int firstDecimal = Integer.parseInt(firstBinary, 2);
+        int secondDecimal = Integer.parseInt(secondBinary, 2);
+        int total = firstDecimal + secondDecimal;
+        return Integer.toBinaryString(total);
+    }
+
+    public String binaryAdd(String firstBinary, String secondBinary){
+        int firstDecimal = Integer.parseInt(firstBinary, 2);
+        int secondDecimal = Integer.parseInt(secondBinary, 2);
+        int total = firstDecimal - secondDecimal;
+        return Integer.toBinaryString(total);
+    }
+
 }
