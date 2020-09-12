343377732db9fdcc4c90332dfaaa89e5036d0e64
ryan-slack <slack.ryanl@gmail.com>
2020-09-11 22:00:13 -0400

    Implemented Main.java to launch application

---
+++ src/edu/bsu/cs/Main.java
@@ -0,0 +1,25 @@
+package edu.bsu.cs;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+            Scene scene = new Scene(root, 300, 320);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- src/edu/bsu/cs/MainController.java
+++ src/edu/bsu/cs/MainController.java
@@ -1,18 +1,4 @@
 package edu.bsu.cs;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.scene.control.TextField;
-import javafx.fxml.FXML;
-
-public class MainController extends Application {
-    @Override
-    public void start(Stage applicationStage) {
-        //TODO: UI code
-    }
-
-    public static void main(String [] args) { launch(args); // Launch application
-    }
+public class MainController {
 }
